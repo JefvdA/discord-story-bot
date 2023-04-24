88e472c49b0656677250a2d4209125d195972312
JefvdA <jef.v.d.a@live.be>
2023-04-24 21:28:46 +0200

    feat: added discord client that connects

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
 from decouple import config
+import discord
+
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+
+@client.event
+async def on_ready():
+    print(f'Logged on as {client.user}')
+
+
+@client.event
+async def on_message(message):
+    print(f'Message from {message.author}: {message.content}')
 
 
 def main():
-    print(config('BOT_TOKEN'))
+    client.run(config('BOT_TOKEN'))
 
 
 if __name__ == '__main__':
