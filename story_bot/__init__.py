c06a5e79a6d1e983a9dc57742f455e28123f4655
JefvdA <jef.v.d.a@live.be>
2023-04-25 22:19:24 +0200

    refactor: extract 'ping' command to PingCog

--- story_bot/__init__.py
+++ story_bot/__init__.py
@@ -1,20 +1,20 @@
 import discord
 from discord.ext import commands
+from story_bot.cogs.ping_cog import PingCog
 
 intents = discord.Intents.default()
 intents.message_content = True
 
 bot = commands.Bot(command_prefix='%', intents=intents)
-tree = bot.tree
 
 
 @bot.event
 async def on_ready():
     print(f'Logged on as {bot.user}')
+
+    print(f'Adding cogs...')
+    await bot.add_cog(PingCog(bot))
+
+    print(f'Syncing commands')
     sync_commands_result = await bot.tree.sync()
     print(f'Synced {len(sync_commands_result)} slash commands.')
-
-
-@tree.command()
-async def ping(interaction: discord.Interaction):
-    await interaction.response.send_message('pong')
---
+++ story_bot/cogs/ping_cog.py
@@ -0,0 +1,13 @@
+import discord
+from discord import app_commands
+from discord.ext import commands
+
+
+class PingCog(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.tree = bot.tree
+
+    @app_commands.command()
+    async def ping(self, interaction: discord.Interaction):
+        await interaction.response.send_message('pong')
