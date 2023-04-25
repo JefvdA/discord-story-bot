2ec1d96e5d026199351bbe5d46e157c3c4143722
JefvdA <jef.v.d.a@live.be>
2023-04-25 21:58:10 +0200

    feat: added 'ping' slash command

--- story_bot/__init__.py
+++ story_bot/__init__.py
@@ -5,13 +5,16 @@ intents = discord.Intents.default()
 intents.message_content = True
 
 bot = commands.Bot(command_prefix='%', intents=intents)
+tree = bot.tree
 
 
 @bot.event
 async def on_ready():
     print(f'Logged on as {bot.user}')
+    sync_commands_result = await bot.tree.sync()
+    print(f'Synced {len(sync_commands_result)} slash commands.')
 
 
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
+@tree.command()
+async def ping(interaction: discord.Interaction):
+    await interaction.response.send_message('pong')
